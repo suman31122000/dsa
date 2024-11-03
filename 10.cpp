cf3e65dc41b58a962d279e3e5ee668f3bede2686
suman bando <sumanbando31122000@gmail.com>
2024-11-03 16:38:29 +0530

    dijkstra

--- 10.cpp
+++ 10.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int INF=INT_MAX;
-vector<int> dijkstra(int v,vector<pair<int,int>>vec[])
+const int INF=1e9+7;
+vector<int>dijkstra(int v,vector<pair<int,int>>vec[])
 {
     vector<int>dist(v+1,INF);
     priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
@@ -9,26 +9,24 @@ vector<int> dijkstra(int v,vector<pair<int,int>>vec[])
     dist[1]=0;
     while(!pq.empty())
     {
-        auto node=pq.top();
-        int cnode=node.second;
-        int  wt=node.first;
+        auto cnode=pq.top();
+        int wt=cnode.first;
+        int node=cnode.second;
         pq.pop();
-
-         if (wt > dist[cnode]) {
-            continue;
-        }
-        for(auto nnode:vec[cnode])
+        if(dist[node]<wt) continue;
+        for(auto nnode:vec[node])
         {
-            int  child=nnode.second;
-            int  nwt=nnode.first;
-            if(dist[cnode]+nwt<dist[child])
+            int child=nnode.first;
+            int nwt=nnode.second;
+            if(dist[node]+nwt<dist[child])
             {
-                dist[child]=dist[cnode]+nwt;
+                dist[child]=dist[node]+nwt;
                 pq.push({dist[child],child});
             }
         }
     }
     return dist;
+
 }
 int main(){
     int v,e;
@@ -36,14 +34,14 @@ int main(){
     vector<pair<int,int>>vec[v+1];
     for(int i=0;i<e;i++)
     {
-      int x,y,wt;
-      cin>>x>>y;
-      vec[x].push_back({wt,y});
-      vec[y].push_back({wt,x});
+        int x,y,wt;
+        cin>>x>>y>>wt;
+        vec[x].push_back({y,wt});
+        vec[y].push_back({x,wt});
     }
-    vector<int>ans=dijkstra(v,vec);
-
-    for(auto it:ans)
-    cout<<it<<" ";
-    cout<<suman<<endl;
+   vector<int>ans= dijkstra(v,vec);
+  for(int i=0;i<ans.size();i++)
+  {
+    cout<<i <<" "<<ans[i]<<endl;
+  }
 }
